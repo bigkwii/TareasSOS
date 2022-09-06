f3c0e96b473d3c5763a8ad57328a5e2eb5d1918d
Álvaro Morales <bigkwii@bigkwii.bigkwii>
2022-09-06 15:58:54 -0400

    more work done on T2

--- T2/bolsa.c
+++ T2/bolsa.c
@@ -11,24 +11,46 @@
 #define FALSE 0
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 
-typedef struct {
-    int lowestPrice; //price
-    char * lowestSeller; //seller name
-    pthread_cond_t w;
-}Request ;
+int prices[]
+char * sellerNames[];
+int lowestPrice = MAX_INT;
 
+int len(int arr[]){
+    //returns the size of an int array
+    return sizeof(arr) / sizeof(* arr);
+}
 
 int vendo(int precio, char *vendedor, char *comprador) {
-  // retorna TRUE si le compran u cuarda nombre de comprador en comprador
-  // returna FALSE si no tiene el precio mas bajo
+  // retorna TRUE si le compran y guarda nombre de comprador en comprador
+  // retorna FALSE si no tiene el precio mas bajo
   
 }
 
 int compro(char *comprador, char *vendedor) {
   // retorna precio del vendedor al que se le compro
   // y guarda el nombre del verdedor en vendedor
-  int precio = 0;
+  // si no hay vendedores, retorna 0
+  int precio;
+  int lowestIdx;
+  pthread_mutex_lock(&m);
+  if(len(prices)>0) precio = MAX_INT;
+  else{
+    pthread_mutex_unlock(&m);
+    return 0;
+  }
+  for(int i=0; i<len(prices);i++) {
+    if(precio > prices[i]) {
+        precio = prices[i];
+        lowestIdx = i;
+        lowestPrice = prices[i];
+    }
+  }
+  vendedor = sellerNames[lowestIdx];
+  pthread_mutex_unlock(&m);
+  pthread_cond_broadcast(&c);
+  return precio
 }
 
 
