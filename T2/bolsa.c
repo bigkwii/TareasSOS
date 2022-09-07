0d72d3570e534117607ba881b01989b32392909e
Álvaro Morales <bigkwii@bigkwii.bigkwii>
2022-09-07 17:33:00 -0400

    figuring this out...

--- T2/bolsa.c
+++ T2/bolsa.c
@@ -9,73 +9,64 @@
 // ...
 #define TRUE 1
 #define FALSE 0
+#define MAX_INT 0x7fffffff
+#define LOCK(m) (pthread_mutex_lock(&m))
+#define UNLOCK(m) (pthread_mutex_unlock(&m))
+#define WAIT(c,m) (pthread_cond_wait(&c,&m))
+#define BROADCAST(c) (pthread_cond_broadcast(&c))
+
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 
-int nSellers = 0; //ammount of sellers
-int nBuyers = 0; // ammount of buyers
-int prices[MAX_INT]; //keeping track of prices
-char * sellerNames[MAX_INT]; //keeping track of names
+// seller stuff
+int thereAreSellers = FALSE;
 int lowestPrice = MAX_INT;
-int lowestPriceInx = 0; //index of the cheapest seller
-char * currentBuyer; //name of the current buyer
-
-
-int popInt(int arr[], int arrLen, int index){
-	// pops the inedx-th element of an int array and returns it.
-	int val = arr[index];
-	for(int i = index; i<arrLen-1; i++)
-		arr[i] = arr[i+1];
-	arrLen--;
-	return val;
-}
-
-void appendInt(int arr[], int arrLen, int val){
-	//append at the end
-	arr[arrLen] = val;
-	arrLen++;
-}
-
-char * popString(char * arr[], int arrLen, int index){
-	// pops the inedx-th element of a string array and returns it.
-	char * val = arr[index];
-	for(int i = index; i<arrLen-1; i++)
-		arr[i] = arr[i+1];
-	arrLen--;
-	return val;
-}
-
-void appendString(char * arr[], int arrLen, char * val){
-	//append at the end
-	arr[arrLen] = val;
-	arrLen++;
-}
+char * lowestPriceName;
+// buyer stuff
+int thereAreBuyers = FALSE;
+char * currentBuyerName;
 
 int vendo(int precio, char *vendedor, char *comprador) {
   // retorna TRUE si le compran y guarda nombre de comprador en comprador
   // retorna FALSE si no tiene el precio mas bajo
-  pthread_mutex_lock(&m);
-  appendInt(prices)
-  pthread_mutex_unlock(&m);
-  while(nBuyers==0){
-  	pthread_mutex_lock(&m);
-  	pthread_cond_wait(&w);
-  	if(precio > lowestPrice)
-  		return FALSE;
-  	pthread_mutex_unlock(&m);
+  LOCK(m);
+  if (precio >= lowestPrice) return FALSE;
+  else {
+  	thereAreSellers = TRUE;
+  	lowestPrice = precio;
+  	lowestPriceName = vendedor;
+  	BROADCAST(c);
   }
-  if(precio > lowestPrice)
-  		return FALSE;
-  
-  
+  while(precio < lowestPrice){
+  	WAIT(c,m);
+  	LOCK(m);
+  	if (precio >= lowestPrice) break;
+  	else {
+  		comprador = currentBuyerName;
+  		thereAreSellers = FALSE;
+  		lowestPrice = MAX_INT;
+  		UNLOCK(m);
+  		BROADCAST(c);
+  		return TRUE;
+  	}
+  }
+  UNLOCK(m);
+  return FALSE;
 }
 
 int compro(char *comprador, char *vendedor) {
   // retorna precio del vendedor al que se le compro
   // y guarda el nombre del verdedor en vendedor
   // si no hay vendedores, retorna 0
-  
+  if(!thereAreSellers) return 0;
+  LOCK(m);
+  currentBuyerName = comprador;
+  vendedor = lowestPriceName;
+  int buyPrice = lowestPrice;
+  UNLOCK(m);
+  BROADCAST(c);
+  return buyPrice;
 }
 
 
