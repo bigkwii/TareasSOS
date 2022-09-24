94c05458dd88c28082b301c4fa59e8afe5bcf3a2
bigkwii <alvaromoralest@gmail.com>
2022-09-24 19:37:40 -0300

    t3 .c file copied

---
+++ T3/disco.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+#include "pss.h"
+#include "disco.h"
+
+// Defina aca sus variables globales
+
+void DiscoInit(void) {
+  // ... inicialice aca las variables globales ...
+}
+
+void DiscoDestroy(void) {
+  // ... destruya las colas para liberar la memoria requerida ...
+}
+
+char *dama(char *nom) {
+  // ...
+}
+
+char *varon(char *nom) {
+  // ...
+}
